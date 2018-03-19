a42c95a64892d569d41978a15d8a88203dc3bed6
Jeppe Traberg Sørensen <J.TrabergS@gmail.com>
2018-03-19 23:35:42 +0100

    Installeret Swagger (swashbuckle)

--- StockShareProvider/Startup.cs
+++ StockShareProvider/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace StockShareProvider
 {
@@ -24,11 +25,20 @@ namespace StockShareProvider
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
