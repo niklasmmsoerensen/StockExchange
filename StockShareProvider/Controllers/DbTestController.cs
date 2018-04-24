cccc78d741813f8b936b9d9c7ba666b2d52b67cd
Jeppe Traberg <J.TrabergS@gmail.com>
2018-04-24 14:29:28 +0200

    Små Db ændringer

---
+++ StockShareProvider/Controllers/DbTestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StockShareProvider.DbAccess;
+using StockShareProvider.DbAccess.Entities;
+
+namespace StockShareProvider.Controllers
+{
+    [Route("api/[controller]")]
+    public class DbTestController : Controller
+    {
+        private readonly ProviderContext _context;
+
+        public DbTestController(ProviderContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("DoTest")]
+        public IActionResult DbTest()
+        {
+            using (var context = _context)
+            {
+                context.SellOrders.Add(new SellOrder());
+                context.SaveChanges();
+
+                var sellorder = context.SellOrders.FirstOrDefault();
+                return sellorder != null ? new ObjectResult(sellorder.ID) : new ObjectResult(0);
+            }
+        }
+    }
+}
--- StockShareProvider/Controllers/StockShareProviderController.cs
+++ StockShareProvider/Controllers/StockShareProviderController.cs
@@ -8,31 +8,10 @@ namespace StockShareProvider.Controllers
     [Route("api/[controller]")]
     public class StockShareProviderController : Controller
     {
-        private readonly ProviderContext _context;
-
-        public TestController(ProviderContext context)
-        {
-            this._context = context;
-        }
-
-
         [HttpGet]
         public string Get()
         {
             return "ayy lmao";
-        }
-
-        [HttpGet("DbTest")]
-        public IActionResult DbTest()
-        {
-            using (var context = _context)
-            {
-                context.SellOrders.Add(new SellOrder());
-                context.SaveChanges();
-
-                var sellorder = context.SellOrders.FirstOrDefault();
-                return sellorder != null ? new ObjectResult(sellorder.ID) : new ObjectResult(0);
-            }
         }
     }
 }
