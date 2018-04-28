6a425085c0937459d59238ab45f8f29490faac75
Jeppe Traberg Sørensen <J.TrabergS@gmail.com>
2018-04-28 23:54:39 +0200

    Navneændringer

--- HTTPGateway/Startup.cs
+++ HTTPGateway/Startup.cs
@@ -28,7 +28,7 @@ namespace HTTPGateway
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "HTTPGateway", Version = "v1" });
             });
 
         }
--- StockShareProvider/Startup.cs
+++ StockShareProvider/Startup.cs
@@ -38,7 +38,7 @@ namespace StockShareProvider
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "StockShareProvider", Version = "v1" });
             });
         }
 
