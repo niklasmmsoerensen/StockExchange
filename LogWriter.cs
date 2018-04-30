6985165e8251e0f46848be563921bb0e29620dab
Marc Holm <marc_holm@live.dk>
2018-04-30 14:40:14 +0200

    Added temporary implementation of logging interface.

---
+++ LogWriter.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Reflection;
+
+public class LogWriter
+{
+    private string _exePath = string.Empty;
+    public LogWriter(string logMessage)
+    {
+        LogWrite(logMessage);
+    }
+    public void LogWrite(string logMessage)
+    {
+        _exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        try
+        {
+            using (StreamWriter w = File.AppendText(_exePath + "\\" + "log.txt"))
+            {
+                Log(logMessage, w);
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    public void LogStart(string logMessage, TextWriter txtWriter)
+    {
+        try
+        {
+            txtWriter.Write("\r\nLog Entry : ");
+            txtWriter.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            txtWriter.WriteLine("  :");
+            txtWriter.WriteLine("  :{0}", logMessage);
+            txtWriter.WriteLine("-------------------------------");
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+}
---
+++ Shared/Logger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Shared
+{
+    public class Logger : ILogger
+    {
+        private static StreamWriter _log;
+
+        public Logger(string microserviceName)
+        {
+            LogStart(microserviceName);
+        }
+
+        public void Debug(string message)
+        {
+            LogString(message, _log);
+        }
+
+        public void Debug(string message, Exception exception)
+        {
+            LogString(message + " " + exception.Message , _log);
+        }
+
+        public void Error(string message)
+        {
+            LogString(message, _log);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            LogString(message + " " + exception.Message + " " + exception.StackTrace, _log);
+        }
+
+        public void Info(string message)
+        {
+            LogString(message, _log);
+        }
+
+        public void Info(string message, Exception exception)
+        {
+            LogString(message + " " + exception.Message, _log);
+        }
+
+        public void Warn(string message)
+        {
+            LogString(message, _log);
+        }
+
+        public void Warn(string message, Exception exception)
+        {
+            LogString(message + " " + exception.Message, _log);
+        }
+
+        private void LogStart(string microservice)
+        {
+            var dateTimeFormat = "MM-dd-yyy_HH_mm_ss";
+            var date = DateTime.Now.ToString(dateTimeFormat);
+            var logFile = "StockExchangeLog " + microservice + " " + date + ".log";
+
+            string logPath = @"c:\Logs";
+            string logFileTest = Path.Combine(logPath, logFile);
+
+            _log = !File.Exists(logFileTest) ? new StreamWriter(logFileTest) : File.AppendText(logFileTest);
+        }
+
+        private void LogString(string stringToLog, StreamWriter log)
+        {
+            log.WriteLine(stringToLog);
+            log.Flush();
+        }
+    }
+}
