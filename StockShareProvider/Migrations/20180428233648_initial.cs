addaa334ae531e0e640e058a9b1e562a9b965536
Jeppe Traberg Sørensen <J.TrabergS@gmail.com>
2018-04-30 15:10:14 +0200

    Fjernet sql fra migration, tilføjet ny connectionstring

--- StockShareProvider/Migrations/20180428233648_initial.cs
+++ StockShareProvider/Migrations/20180428233648_initial.cs
@@ -21,11 +21,6 @@ namespace StockShareProvider.Migrations
                 {
                     table.PrimaryKey("PK_SellOrders", x => x.ID);
                 });
-
-          
-            migrationBuilder.Sql(
-                "USE [Provider]\r\nGO\r\n\r\nCREATE USER [NT AUTHORITY\\NETWORK SERVICE] FOR LOGIN [NT AUTHORITY\\NETWORK SERVICE] WITH DEFAULT_SCHEMA=[db_owner]\r\nGO");
-            migrationBuilder.Sql("ALTER ROLE db_owner\r\nADD MEMBER [NT AUTHORITY\\NETWORK SERVICE]");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
